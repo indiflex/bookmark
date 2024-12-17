c47721b4b598c8251436b708d6a28706ae968f13
indiflex <indiflex1@gmail.com>
2024-12-17 20:43:47 +0900

    lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,43 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
-export default [...compat.extends("next/core-web-vitals", "next/typescript")];
+
+const config = [
+  ...ts.configs.recommended,
+  { ignores: ['.next/*'] },
+
+  ...fixupConfigRules(
+    compat.extends(
+      'next/core-web-vitals',
+      'prettier',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended'
+    )
+  ),
+  {
+    plugins: {
+      react: fixupPluginRules(react),
+      'react-hooks': fixupPluginRules(reactHooks),
+    },
+
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+];
+
+export default config;
